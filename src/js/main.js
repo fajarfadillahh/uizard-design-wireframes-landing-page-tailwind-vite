5b3f2716ecb1ca321e65b6392c7512de4ab8aa52
fajarfadillahh <fajaragustian979@gmail.com>
2022-08-06 19:40:40 +0700

    header section

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,14 @@
 import "../css/index.css";
+
+// ===== SHOW MENU =====
+function shoewMenu(menuId, toggleId) {
+  const menu = document.getElementById(menuId);
+  const toggle = document.getElementById(toggleId);
+
+  if (menu && toggle) {
+    toggle.addEventListener("click", () => {
+      menu.classList.toggle("show-menu");
+    });
+  }
+}
+shoewMenu("header-menu", "header-toggle");
